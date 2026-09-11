7ff2f4a5fe3e010fa81e4c4b86c29eb95906ccbe
vtu30166-blip <vtu30166@veltech.edu.in>
2026-09-11 12:13:00 +0530

    Implement Arithmetic and Adder classes

---
+++ java Iheritance2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+//Write your code here
+// Write your code here
+class Arithmetic {
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+    // This class inherits the add method from the Arithmetic class
+}
+
+public class Solution{
