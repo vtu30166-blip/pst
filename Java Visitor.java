ce1b76b195aa0fc1bf481a8c4546131177e02c6d
vtu30166-blip <vtu30166@veltech.edu.in>
2026-09-16 11:30:58 +0530

    Implement Tree structure with Visitor pattern

---
+++ Java Visitor.java
@@ -0,0 +1,219 @@
+mport java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+enum Color {
+    RED, GREEN
+}
+
+abstract class Tree {
+
+    private int value;
+    private Color color;
+    private int depth;
+
+    public Tree(int value, Color color, int depth) {
+        this.value = value;
+        this.color = color;
+        this.depth = depth;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public abstract void accept(TreeVis visitor);
+}
+
+class TreeNode extends Tree {
+
+    private ArrayList<Tree> children = new ArrayList<>();
+
+    public TreeNode(int value, Color color, int depth) {
+        super(value, color, depth);
+    }
+
+    public void accept(TreeVis visitor) {
+        visitor.visitNode(this);
+
+        for (Tree child : children) {
+            child.accept(visitor);
+        }
+    }
+
+    public void addChild(Tree child) {
+        children.add(child);
+    }
+}
+
+class TreeLeaf extends Tree {
+
+    public TreeLeaf(int value, Color color, int depth) {
+        super(value, color, depth);
+    }
+
+    public void accept(TreeVis visitor) {
+        visitor.visitLeaf(this);
+    }
+}
+
+abstract class TreeVis
+{
+    public abstract int getResult();
+    public abstract void visitNode(TreeNode node);
+    public abstract void visitLeaf(TreeLeaf leaf);
+
+}
+class SumInLeavesVisitor extends TreeVis {
+
+    private int sum = 0;
+
+    public int getResult() {
+        return sum;
+    }
+
+    public void visitNode(TreeNode node) {
+        // Do nothing
+    }
+
+    public void visitLeaf(TreeLeaf leaf) {
+        sum += leaf.getValue();
+    }
+}
+
+class ProductOfRedNodesVisitor extends TreeVis {
+
+    private long product = 1;
+    private static final long MOD = 1000000007;
+
+    public int getResult() {
+        return (int) product;
+    }
+
+    public void visitNode(TreeNode node) {
+        if (node.getColor() == Color.RED) {
+            product = (product * node.getValue()) % MOD;
+        }
+    }
+
+    public void visitLeaf(TreeLeaf leaf) {
+        if (leaf.getColor() == Color.RED) {
+            product = (product * leaf.getValue()) % MOD;
+        }
+    }
+}
+
+class FancyVisitor extends TreeVis {
+
+    private int evenDepthSum = 0;
+    private int greenLeafSum = 0;
+
+    public int getResult() {
+        return Math.abs(evenDepthSum - greenLeafSum);
+    }
+
+    public void visitNode(TreeNode node) {
+        if (node.getDepth() % 2 == 0) {
+            evenDepthSum += node.getValue();
+        }
+    }
+
+    public void visitLeaf(TreeLeaf leaf) {
+        if (leaf.getColor() == Color.GREEN) {
+            greenLeafSum += leaf.getValue();
+        }
+    }
+}
+
+public class Solution {
+
+    public static Tree solve() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] values = new int[n];
+        for (int i = 0; i < n; i++) {
+            values[i] = sc.nextInt();
+        }
+
+        int[] colors = new int[n];
+        for (int i = 0; i < n; i++) {
+            colors[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer>[] graph = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int u = sc.nextInt() - 1;
+            int v = sc.nextInt() - 1;
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        return buildTree(0, -1, 0, values, colors, graph);
+    }
+
+    private static Tree buildTree(
+            int current,
+            int parent,
+            int depth,
+            int[] values,
+            int[] colors,
+            ArrayList<Integer>[] graph) {
+
+        Color color = colors[current] == 0 ? Color.RED : Color.GREEN;
+
+        boolean isLeaf = true;
+
+        for (int next : graph[current]) {
+            if (next != parent) {
+                isLeaf = false;
+                break;
+            }
+        }
+
+        if (isLeaf) {
+            return new TreeLeaf(values[current], color, depth);
+        }
+
+        TreeNode node = new TreeNode(values[current], color, depth);
+
+        for (int next : graph[current]) {
+            if (next != parent) {
+                node.addChild(
+                    buildTree(
+                        next,
+                        current,
+                        depth + 1,
+                        values,
+                        colors,
+                        graph
+                    )
+                );
+            }
+        }
+
+        return node;
+    }
+
+
